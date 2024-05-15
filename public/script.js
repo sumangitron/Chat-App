1bc1a671dea574ea5e8698c6906859cc83aa436f
sumangitron <sumanabatar1994@gmail.com>
2024-05-15 23:51:55 +0530

    complete

--- public/script.js
+++ public/script.js
@@ -3,86 +3,81 @@ const socket = io(); //establish a connection
 let username = "";
 
 document.querySelector("#join-btn").addEventListener("click", (event) => {
-    event.preventDefault();
+  event.preventDefault();
 
-    let input = document.querySelector("#username-input");
-    username = input.value;
-    if(!username) {
-        alert("Please enter a usename");
-    }
-    else {
-        //enter my chat
-        let form = document.querySelector(".form-username");
-        form.style.display = "none";
-        let chatContainer = document.querySelector(".chatroom-container");
-        chatContainer.style.display = "block";
+  let input = document.querySelector("#username-input");
+  username = input.value;
+  if (!username) {
+    alert("Please enter a usename");
+  } else {
+    //enter my chat
+    let form = document.querySelector(".form-username");
+    form.style.display = "none";
+    let chatContainer = document.querySelector(".chatroom-container");
+    chatContainer.style.display = "block";
 
-        document.querySelector(".chatroom-header").textContent = `Chatroom - ${username}`;
+    document.querySelector(
+      ".chatroom-header"
+    ).textContent = `Chatroom - ${username}`;
 
-        // username = "";
+    // username = "";
 
-        socket.emit("joined", username);
-    }
-})
+    socket.emit("joined", username);
+    input.value = "";
+  }
+});
 
 document.getElementById("send-btn").addEventListener("click", () => {
-    let message = document.getElementById("message-input").value.trim();
-    // message = "";
-    socket.emit("messageSent", { message, username });
-})
+  let message = document.getElementById("message-input").value.trim();
+  // message = "";
+  socket.emit("messageSent", { message, username });
+  message.value = "";
+});
 
 document.querySelector("#exit-btn").addEventListener("click", () => {
-    socket.emit("exit", username);
+  socket.emit("exit", username);
 
-    let form = document.querySelector(".form-username");
-    form.style.display = "block";
-    let chatContainer = document.querySelector(".chatroom-container");
-    chatContainer.style.display = "none";
-})
+  let form = document.querySelector(".form-username");
+  form.style.display = "block";
+  let chatContainer = document.querySelector(".chatroom-container");
+  chatContainer.style.display = "none";
+});
 
 socket.on("messageSent", (data) => {
-    console.log(data);
-    if(data.username === username) {
-        //message is mine
-        showMessage(data, true);
-    }
-    else {
-        //message is someone else
-        showMessage(data, false);
-    }
-})
+  console.log(data);
+  if (data.username === username) {
+    //message is mine
+    showMessage(data, true);
+  } else {
+    //message is someone else
+    showMessage(data, false);
+  }
+});
 
 function showMessage(message, mine) {
-    if(mine) {
-        document.getElementById("messages-container").innerHTML +=
-            `
+  if (mine) {
+    document.getElementById("messages-container").innerHTML += `
             <div class='message sent'>${message.username}: ${message.message}</div>
-            `
-    }
-    else {
-        document.getElementById("messages-container").innerHTML +=
-            `
+            `;
+  } else {
+    document.getElementById("messages-container").innerHTML += `
             <div class='message received'>${message.username}: ${message.message}</div>
-            `
-        }
+            `;
+  }
 }
 
 socket.on("joined", (newUser) => {
-    if(newUser !== username) {
-        document.getElementById("messages-container").innerHTML +=
-            `
-            <p>${newUser} has joined!</p>
-            `
-    }
-})
+  if (newUser !== username) {
+    document.getElementById("messages-container").innerHTML += `
+            <p class='chip'>${newUser} has joined!</p>
+            `;
+  }
+});
 
 socket.on("exit", (leftUser) => {
-    if(leftUser !== username) {
-        document.getElementById("messages-container").innerHTML +=
-            `
-            <p>${leftUser} has left!</p>
-            `
-    }
-
-    
-})
+  if (leftUser !== username) {
+    document.getElementById("messages-container").innerHTML += `
+            <p class='chip'>${leftUser} has left!</p>
+            `;
+  }
+});
